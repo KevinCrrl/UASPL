5dae1d0e7f3687a4010ac1fe511cfff0d41bf5df
KevinCrrl <y62257136@gmail.com>
2025-05-27 10:37:56 -0500

    Corrigiendo enlace de la web en la wiki

--- herramientas/idioma.py
+++ herramientas/idioma.py
@@ -14,5 +14,5 @@ if os.path.exists(ruta): # Verifico que la ruta exista porque si no existe y el 
 
 else:
     from idiomas.espanol import palabras
-    print("""No se cargó ningún idioma, se usará español por defecto, para cambiar el idioma o dejar de ver este mensaje, cree el archivo config.json de acuerdo a las instrucciones dadas en https://wiki.archlinux.org/title/Uaspl-bin
-No language loaded, Spanish will be used by default, to change the language or stop seeing this message, create the config.json file according to the instructions given at https://wiki.archlinux.org/title/Uaspl-bin""")
+    print("""No se cargó ningún idioma, se usará español por defecto, para cambiar el idioma o dejar de ver este mensaje, cree el archivo config.json de acuerdo a las instrucciones dadas en https://wiki.archlinux.org/title/User:KevinCrrl/Uaspl-bin_(Espa%C3%B1ol)
+No language loaded, Spanish will be used by default, to change the language or stop seeing this message, create the config.json file according to the instructions given at https://wiki.archlinux.org/title/User:KevinCrrl/Uaspl-bin_(Espa%C3%B1ol)""")
