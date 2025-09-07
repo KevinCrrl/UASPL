41a122176d5696912b93afbface61120398ce730
KevinCrrl <y62257136@gmail.com>
2025-09-07 14:23:57 -0500

    pequeñas mejoras para el empaquetado

--- src/uaspl/setup.py
+++ src/uaspl/setup.py
@@ -1,7 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="uaspl",
     version="1.2.2",
+    packages=find_packages(),
     py_modules=["UASPL"],
+    include_package_data=True,
 )
